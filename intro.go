28150dd30a6f3c664770f01e20bbca72667af22a
WahomeKennedy <kennedywahome.24@gmail.com>
2024-07-14 14:21:46 +0300

    Learnt types, and syntax

--- intro.go
+++ intro.go
@@ -1,7 +1,22 @@
-package main
+package main // package name. Our Go application should start with package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello World!")
+func main() { // main function. The entry point of our Go application
+	var conferenceName string = "GopherCon"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)                                      // fmt or format is a package that provides I/O functions
+	fmt.Printf("We have a total of %v and %v are still available.\n", conferenceTickets, remainingTickets) //we use Printf when we want to combine the variables with the string.
+	fmt.Println("Get your tickets now to attend")
+
+	var userName string
+	var userAge int
+
+	userName = "Tom"
+	userAge = 25
+
+	fmt.Println("User Name: ", userName)
+	fmt.Println("User Age: ", userAge)
+
 }
