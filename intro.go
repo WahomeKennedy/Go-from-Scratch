5101a60a5dfe3e37eef7e028433e828823bc29bb
WahomeKennedy <kennedywahome.24@gmail.com>
2024-09-15 10:05:26 +0300

    Go hello world

--- intro.go
+++ intro.go
@@ -1,23 +1,7 @@
-package main // package name. Our Go application should start with package main
+package main
 
 import "fmt"
 
-func main() { // main function. The entry point of our Go application
-	var conferenceName string = "GopherCon"
-	const conferenceTickets = 50
-	var remainingTickets = 50
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)                                      // fmt or format is a package that provides I/O functions
-	fmt.Printf("We have a total of %v and %v are still available.\n", conferenceTickets, remainingTickets) //we use Printf when we want to combine the variables with the string.
-	fmt.Println("Get your tickets now to attend")
-
-	var userName string
-	var userTickets int
-
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&userName)
-
-	userTickets = 25
-
-	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
-
+func main() {
+	fmt.Println("Hello, World!")
 }
